ca356dca20e1d84b7a71a5ded5f1b947251c5554
Igor <iroztropinski@gmail.com>
2022-11-09 18:23:16 +0100

    Set up client and env variables

--- src/docker_logs_collector.py
+++ src/docker_logs_collector.py
@@ -2,6 +2,7 @@ import json
 import logging
 import random
 import time
+from os import environ
 
 import docker
 
@@ -10,7 +11,19 @@ logging.basicConfig(level=logging.INFO,
                     datefmt="%Y-%m-%d %H:%M:%S")
 LOG = logging.getLogger(__file__)
 
-machine_name = "virtuocrat"
+ID_FIELD = "id"
+NAME_FIELD = "name"
+
+machine_name = environ.get("MACHINE_NAME", "virtuocrat")
+
+logs_target_url = environ.get('LOGS_TARGET_URL', "http://localhost:5555")
+logs_target_headers = environ.get("LOGS_TARGET_HEADERS", {})
+if logs_target_headers:
+    logs_target_headers = {h.split("=")[0].strip(): h.split("=")[1].strip() for h in logs_target_headers.split(",")}
+
+collection_interval = int(environ.get("COLLECTION_INTERVAL", 10))
+LAST_DATA_READ_AT_FILE_PATH = environ.get("LAST_DATA_READ_AT_FILE",
+                                          "/tmp/docker-logs-collector-last-data-read-at.txt")
 
 
 def connected_docker_client_retrying():
@@ -44,9 +57,21 @@ def log_exception(message):
     print()
 
 
+def current_timestamp():
+    return int(time.time())
+
+
+def current_timestamp_millis():
+    return int(time.time() * 1000)
+
+
 docker_client = connected_docker_client_retrying()
 
 running_containers = docker_client.containers()
 
-for c in running_containers:
-    print(c)
+# for c in running_containers:
+#     print(c)
+#     c_id = c['Id']
+#     logs = docker_client.logs(c_id, since=current_timestamp() - 10_000, until=current_timestamp(), stream=False).decode(
+#         "utf-8")
+#     print(logs)
